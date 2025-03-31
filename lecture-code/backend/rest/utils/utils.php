0a725851ef94bbb2be664af5a17b3e459e5abb2e
ibu-web-programming2001 <web2001programming@gmail.com>
2025-03-31 14:45:33 +0200

    Add files via upload

--- lecture-code/backend/rest/utils/utils.php
+++ lecture-code/backend/rest/utils/utils.php
@@ -1,1 +1,16 @@
+<?php
 
+class Utils {
+
+    public function validateField($array, $field){
+        if(is_null($array[$field])){
+            return false;
+        }
+
+        if ($field == "first_name"){
+            ///
+        }
+
+        return true;
+    }
+}
