f42db3d1ac7393db8d3e243bebe5de5cfb044fbe
ibu-web-programming2001 <web2001programming@gmail.com>
2025-04-09 10:15:45 +0200

    Update swagger.php

--- lecture-code/backend/public/v1/docs/swagger.php
+++ lecture-code/backend/public/v1/docs/swagger.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
+define('LOCALSERVER', 'http://localhost:83/project/backend/');
+define('PRODSERVER', 'https://add-production-server-after-deployment/backend/');
+
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost:83/project/backend/');
 } else {
