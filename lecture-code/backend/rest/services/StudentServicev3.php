2044d71e09a1e334ab30f64ab75c045e817d3602
ibu-web-programming2001 <web2001programming@gmail.com>
2025-04-23 17:40:22 +0200

    Update StudentServicev3.php

--- lecture-code/backend/rest/services/StudentServicev3.php
+++ lecture-code/backend/rest/services/StudentServicev3.php
@@ -8,4 +8,18 @@ class StudentServicev3 extends BaseService
     {
         parent::__construct(new StudentDaov4);
     }
+
+    public function add($entity)
+    {
+        $is_email_existing = $this->dao->get_by_email($entity["email"]);
+        if($is_email_existing){
+            return [];
+        }
+
+        return parent::add($entity);
+    }
+
+    public function get_department_students($department_id){
+        return $this->dao->get_by_department($department_id);
+    }
 }
