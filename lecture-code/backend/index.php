c569fd151bb228a7c73ed3b3f35efd61b80e3031
ibu-web-programming2001 <web2001programming@gmail.com>
2025-10-27 16:24:06 +0100

    Update index.php

--- lecture-code/backend/index.php
+++ lecture-code/backend/index.php
@@ -2,35 +2,14 @@
 require "./vendor/autoload.php";
 require "rest/services/StudentServicev3.php";
 require "rest/services/ProfessorServicev1.php";
-require "rest/services/AuthService.php";
-require "middleware/AuthMiddleware.php";
-
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
 
 Flight::register('student_service', "StudentServicev3");
 Flight::register('professor_service', "ProfessorServicev1");
-Flight::register('auth_service', "AuthService");
-Flight::register('auth_middleware', "AuthMiddleware");
 
 Flight::route('/*', function() {
-    if(
-        strpos(Flight::request()->url, '/auth/login') === 0 ||
-        strpos(Flight::request()->url, '/auth/register') === 0
-    ) {
-        return TRUE;
-    } else {
-        try {
-            $token = Flight::request()->getHeader("Authentication");
-            if(Flight::auth_middleware()->verifyToken($token))
-                return TRUE;
-        } catch (\Exception $e) {
-            Flight::halt(401, $e->getMessage());
-        }
-    }
+    return TRUE;
 });
 
-require_once 'rest/routes/AuthRoutes.php';
 require_once 'rest/routes/StudentRoutes.php';
 require_once 'rest/routes/ProfessorRoutes.php';
 
